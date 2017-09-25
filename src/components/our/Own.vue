a8d13540dacf428f64fc44070514bcae1952ad1f
zhangyanan0819 <1573904839@qq.com>
2017-09-25 11:10:56 +0800

    ma

--- src/components/our/Own.vue
+++ src/components/our/Own.vue
@@ -1,5 +1,19 @@
 <template>
-    <section>jhkufhaufh</section>
+  <section class="my-main">
+    <section class="login">
+      <div class="my-login">
+        <img src="" alt="">
+        <a href="">点击进入</a>
+      </div>
+      <div class="position-box">
+        盒子
+      </div>
+    </section>
+    <section class="list-main">
+      <mt-cell title="标题" label="描述信息" is-link></mt-cell>
+    </section>
+    <section class="user-message"></section>
+  </section>
 </template>
 
 <script>
