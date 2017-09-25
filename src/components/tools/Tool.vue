a1be6bb4bc385c862573677e5288d69c22cf178f
ethan <137322837@qq.com>
2017-09-25 16:27:54 +0800

    tool1.0

--- src/components/tools/Tool.vue
+++ src/components/tools/Tool.vue
@@ -1,11 +1,111 @@
 <template>
-    <section>154846558</section>
+    <div>
+      <header>
+        <span>福居工具</span>
+      </header>
+      <section>
+        <mt-swipe :auto="3000" @change="handleChange">
+          <mt-swipe-item><img src="./images/2a.jpg" alt="" height="230px" width="100%"></mt-swipe-item>
+          <mt-swipe-item><img src="./images/2a.jpg" alt="" height="230px" width="100%"></mt-swipe-item>
+          <mt-swipe-item><img src="./images/2a.jpg" alt="" height="230px" width="100%"></mt-swipe-item>
+        </mt-swipe>
+        <ul class="icon-box">
+          <li>
+            <a href="">
+              <div class="icon"><img src="./images/download-3.png" alt=""></div>
+              <div>经纪人</div>
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <div class="icon"><img src="./images/download-5.png" alt=""></div>
+              <div>房贷计算器</div>
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <div class="icon"><img src="./images/download.png" alt=""></div>
+              <div>税费计算</div>
+            </a>
+          </li>
+        </ul>
+        <ul class="icon-box">
+          <li>
+            <a href="">
+              <div class="icon"><img src="./images/download-2.png" alt=""></div>
+              <div>查门店</div>
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <div class="icon"><img src="./images/download-4.png" alt=""></div>
+              <div>房屋估价</div>
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <div class="icon"><img src="./images/download-1.png" alt=""></div>
+              <div>帮我找房</div>
+            </a>
+          </li>
+        </ul>
+      </section>
+    </div>
 </template>
 
 <script>
+  export default {
+    data () {
 
+    },
+    methods: {
+      handleChange (index) {
+
+      }
+    }
+  }
 </script>
 
 <style>
-
+  *{
+    margin: 0;
+    padding: 0;
+  }
+  header{
+    text-align: center;
+    padding: 10px;
+  }
+  header span {
+    height: 50px;
+    font-size: 18px;
+  }
+  .icon-box {
+    display: flex;
+    list-style: none;
+    padding: 10px;
+  }
+  .icon-box li{
+    flex: 1;
+    padding: 5px;
+  }
+  .icon-box a{
+    border: 1px solid rgb(240,240,240);
+    box-shadow: 0 0 8px rgb(240,240,240);
+    color: darkslateblue;
+    display: inline-block;
+    width: 100px;
+    height: 100px;
+    text-align: center;
+    text-decoration: none;
+  }
+  .icon-box .icon  {
+    text-align: center;
+  }
+  .icon-box .icon img{
+    height: 40px;
+    margin: 12px;
+  }
+  .mint-swipe {
+    height: 230px;
+  }
 </style>
